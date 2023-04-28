22383a02ea4d581c23cfaf2fcc8d3b2f0accff35
sumin <ujou173@gmail.com>
2023-04-28 12:14:59 +0900

    commander를 이용한 HTML 문서 생성기

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
-import inquirer from 'inquirer';
-const { Command } = require('commander');
+// import inquirer from 'inquirer';
+const { program } = require('commander');
+const fs = require('fs');
 
-const commander  = new Command();
-
-commander
-  .version('V 0.0.1')
-  .description('CLI Interface')
-  .option('-f, --filename <filename>', 'set filename')
+program
+  .option('-f, --name <name>', 'set name')
   .option('-t, --title <title>', 'set title')
-  .option('-p, --content <content>', 'set content')
+  .option('-c, --content <content>', 'set content')
+  .parse(process.argv)
 
-  // commander.Command('split')
+const value = program.opts();
+console.log(value);
+
+createHTML(value)
+
+function createHTML(data) {
+  const fileName = data.name;
+  const title = data.title;
+  const content = `<div id='root'>
+    <p>${data.content}</p>
+  </div>`;
+
+  const bodyData = `
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>${title}</title>
+</head>
+<body>
+  ${content}
+</body>
+</html>
+`;
+
+  fs.writeFile(`./result/${fileName}.html`, bodyData, (err) => {
+    if (err) throw err;
+    console.log('파일이 생성되었습니다')
+  })
+}
