b616b8636abf6e1aeb38a1067ecaf62b798d6cd8
sumin <ujou173@gmail.com>
2023-04-28 10:13:13 +0900

    파일명 입력 확인

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 import inquirer from 'inquirer';
+
+const fileName = {
+  type: 'input',
+  name: 'name',
+  message: '작성 될 HTML 파일의 이름을 입력하세요',
+}
+
+inquirer.prompt(fileName).then(answer => {
+  console.log(answer);
+})
